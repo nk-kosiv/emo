17c747921a4dd254ed2299a92dd8235cadc263b1
nk-kosiv <nazrkosiv777@gmail.com>
2020-02-18 20:46:34 +0200

    Updates

---
+++ precache-manifest.7a9f8b747bf1003f3f2b24b08093b627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b5fcd987d0446474ff949f1ec2b217",
+    "url": "/emo/index.html"
+  },
+  {
+    "revision": "895a03f1676d7251b03e",
+    "url": "/emo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c2c86064f209d61efe1d",
+    "url": "/emo/static/js/2.a049f7c5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/emo/static/js/2.a049f7c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895a03f1676d7251b03e",
+    "url": "/emo/static/js/main.68116796.chunk.js"
+  },
+  {
+    "revision": "3b693ced1318473c39db",
+    "url": "/emo/static/js/runtime-main.1d690946.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/emo/static/media/logo.5d5d9eef.svg"
+  }
+]);
